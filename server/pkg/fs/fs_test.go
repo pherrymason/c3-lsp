2ca9901ae3180b64586dcf9189d56d0c22734d77
Raúl Ferràs <raulferras@gmail.com>
2024-08-03 21:25:49 +0200

    Fix test

--- server/pkg/fs/fs_test.go
+++ server/pkg/fs/fs_test.go
@@ -11,5 +11,5 @@ func TestConvertPathToURI(t *testing.T) {
 
 	uri := ConvertPathToURI(path)
 
-	assert.Equal(t, "file://D:/projects/c3-lsp/assets/c3-demo/foobar/foo.c3", uri)
+	assert.Equal(t, "file:///D:/projects/c3-lsp/assets/c3-demo/foobar/foo.c3", uri)
 }
