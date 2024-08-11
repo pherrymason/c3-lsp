bc97b09c29cc0b13a1c0d70abce7c5190183e4ff
Raúl Ferràs <raulferras@gmail.com>
2024-08-11 17:56:24 +0200

    Convert assignment expressions

---
+++ server/internal/lsp/ast/convert_ct.go
@@ -0,0 +1,96 @@
+package ast
+
+import sitter "github.com/smacker/go-tree-sitter"
+
+func convert_ct_type_ident(node *sitter.Node, source []byte) Expression {
+	return Literal{Value: node.Content(source)}
+}
+
+/*
+"$alignof",
+
+	"$extnameof",
+	"$nameof",
+	"$offsetof",
+	"$qnameof"
+*/
+func convert_compile_time_call(node *sitter.Node, source []byte) Expression {
+	// seq($._ct_call, '(', $.flat_path, ')'),
+	endNode := node.NextSibling()
+	for {
+		n := endNode.NextSibling()
+
+		if n == nil {
+			break
+		}
+		endNode = n
+	}
+
+	flatPath := node.NextNamedSibling()
+	endNode = flatPath.NextSibling()
+
+	funcCall := FunctionCall{
+		ASTNodeBase: NewBaseNodeBuilder().
+			WithSitterPosRange(node.StartPoint(), endNode.EndPoint()).
+			Build(),
+		Identifier: NewIdentifierBuilder().
+			WithName(node.Content(source)).
+			WithSitterPos(node).
+			Build(),
+		Arguments: []Arg{convert_flat_path(flatPath, source)},
+	}
+
+	return funcCall
+}
+
+func convert_compile_time_arg(node *sitter.Node, source []byte) Expression {
+	endNode := node
+	var insideParenths *sitter.Node
+	for {
+		n := endNode.NextSibling()
+		endNode = n
+		if n.Type() != "(" && n.Type() != ")" {
+			insideParenths = n
+		}
+		if n.Type() == ")" {
+			break
+		}
+	}
+
+	expr := convert_expression(insideParenths, source)
+
+	funcCall := FunctionCall{
+		ASTNodeBase: NewBaseNodeBuilder().
+			WithSitterPosRange(node.StartPoint(), endNode.EndPoint()).
+			Build(),
+		Identifier: NewIdentifierBuilder().
+			WithName(node.Content(source)).
+			WithSitterPos(node).
+			Build(),
+		Arguments: []Arg{expr},
+	}
+
+	return funcCall
+}
+
+func convert_compile_analyse(node *sitter.Node, source []byte) Expression {
+	// comma_decl_or_expr
+	debugNode(node, source)
+	decl_or_expr_node := node.NextNamedSibling()
+	debugNode(decl_or_expr_node, source)
+
+	expressions := convert_token_separated(decl_or_expr_node, ",", source, convert_decl_or_expr)
+
+	funcCall := FunctionCall{
+		ASTNodeBase: NewBaseNodeBuilder().
+			WithSitterPosRange(node.StartPoint(), decl_or_expr_node.NextSibling().EndPoint()).
+			Build(),
+		Identifier: NewIdentifierBuilder().
+			WithName(node.Content(source)).
+			WithSitterPos(node).
+			Build(),
+		Arguments: cast_expressions_to_args(expressions),
+	}
+
+	return funcCall
+}
