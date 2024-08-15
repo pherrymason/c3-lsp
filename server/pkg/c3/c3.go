e6d538ca0039c7b787cf0033369ca5252b369dde
Nikita Pivkin <nikita.pivkin@smartforce.io>
2024-08-15 23:57:24 +0600

    store language keywords in a set instead of a slice
    Signed-off-by: Nikita Pivkin <nikita.pivkin@smartforce.io>

--- server/pkg/c3/c3.go
+++ server/pkg/c3/c3.go
@@ -1,38 +1,35 @@
 package c3
 
-func IsLanguageKeyword(symbol string) bool {
-	keywords := []string{
-		"void", "bool", "char", "double",
-		"float", "float16", "int128", "ichar",
-		"int", "iptr", "isz", "long",
-		"short", "uint128", "uint", "ulong",
-		"uptr", "ushort", "usz", "float128",
-		"any", "anyfault", "typeid", "assert",
-		"asm", "bitstruct", "break", "case",
-		"catch", "const", "continue", "def",
-		"default", "defer", "distinct", "do",
-		"else", "enum", "extern", "false",
-		"fault", "for", "foreach", "foreach_r",
-		"fn", "tlocal", "if", "inline",
-		"import", "macro", "module", "nextcase",
-		"null", "return", "static", "struct",
-		"switch", "true", "try", "union",
-		"var", "while",
+var keywords = map[string]struct{}{
+	"void": {}, "bool": {}, "char": {}, "double": {},
+	"float": {}, "float16": {}, "int128": {}, "ichar": {},
+	"int": {}, "iptr": {}, "isz": {}, "long": {},
+	"short": {}, "uint128": {}, "uint": {}, "ulong": {},
+	"uptr": {}, "ushort": {}, "usz": {}, "float128": {},
+	"any": {}, "anyfault": {}, "typeid": {}, "assert": {},
+	"asm": {}, "bitstruct": {}, "break": {}, "case": {},
+	"catch": {}, "const": {}, "continue": {}, "def": {},
+	"default": {}, "defer": {}, "distinct": {}, "do": {},
+	"else": {}, "enum": {}, "extern": {}, "false": {},
+	"fault": {}, "for": {}, "foreach": {}, "foreach_r": {},
+	"fn": {}, "tlocal": {}, "if": {}, "inline": {},
+	"import": {}, "macro": {}, "module": {}, "nextcase": {},
+	"null": {}, "return": {}, "static": {}, "struct": {},
+	"switch": {}, "true": {}, "try": {}, "union": {},
+	"var": {}, "while": {},
 
-		"$alignof", "$assert", "$case", "$default",
-		"$defined", "$echo", "$embed", "$exec",
-		"$else", "$endfor", "$endforeach", "$endif",
-		"$endswitch", "$eval", "$evaltype", "$error",
-		"$extnameof", "$for", "$foreach", "$if",
-		"$include", "$nameof", "$offsetof", "$qnameof",
-		"$sizeof", "$stringify", "$switch", "$typefrom",
-		"$typeof", "$vacount", "$vatype", "$vaconst",
-		"$varef", "$vaarg", "$vaexpr", "$vasplat",
-	}
-	for _, w := range keywords {
-		if w == symbol {
-			return true
-		}
-	}
-	return false
+	"$alignof": {}, "$assert": {}, "$case": {}, "$default": {},
+	"$defined": {}, "$echo": {}, "$embed": {}, "$exec": {},
+	"$else": {}, "$endfor": {}, "$endforeach": {}, "$endif": {},
+	"$endswitch": {}, "$eval": {}, "$evaltype": {}, "$error": {},
+	"$extnameof": {}, "$for": {}, "$foreach": {}, "$if": {},
+	"$include": {}, "$nameof": {}, "$offsetof": {}, "$qnameof": {},
+	"$sizeof": {}, "$stringify": {}, "$switch": {}, "$typefrom": {},
+	"$typeof": {}, "$vacount": {}, "$vatype": {}, "$vaconst": {},
+	"$varef": {}, "$vaarg": {}, "$vaexpr": {}, "$vasplat": {},
+}
+
+func IsLanguageKeyword(symbol string) bool {
+	_, exists := keywords[symbol]
+	return exists
 }
