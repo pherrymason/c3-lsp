dd311d16ce9873e76cdc9a22ef3dd34c09dae854
Raúl Ferràs <raulferras@gmail.com>
2025-01-28 18:28:54 +0100

    Entry point for Document Completion.

--- server/internal/lsp/server/TextDocumentCompletion.go
+++ server/internal/lsp/server/TextDocumentCompletion.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"github.com/pherrymason/c3-lsp/internal/lsp"
+	"github.com/pherrymason/c3-lsp/internal/lsp/analysis"
 	ctx "github.com/pherrymason/c3-lsp/internal/lsp/context"
+	"github.com/pherrymason/c3-lsp/pkg/featureflags"
 	"github.com/pherrymason/c3-lsp/pkg/utils"
 	"github.com/tliron/glsp"
 	protocol "github.com/tliron/glsp/protocol_3_16"
@@ -10,6 +13,21 @@ import (
 // Support "Completion"
 // Returns: []CompletionItem | CompletionList | nil
 func (srv *Server) TextDocumentCompletion(context *glsp.Context, params *protocol.CompletionParams) (any, error) {
+	if featureflags.IsActive(featureflags.UseGeneratedAST) {
+		doc, _ := srv.documents.GetDocument(params.TextDocument.URI)
+		completionList := analysis.BuildCompletionList(
+			doc,
+			lsp.NewLSPPosition(params.Position),
+			srv.documents,
+			srv.symbolTable,
+		)
+
+		return completionList, nil
+	}
+
+	// -----------------------
+	// Old implementation
+	// -----------------------
 
 	cursorContext := ctx.BuildFromDocumentPosition(
 		params.Position,
